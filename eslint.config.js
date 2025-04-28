9e7d81f9cb6f1d31f5dc255cdad5e60974083175
ricnaga <ric.nagato@gmail.com>
2025-04-28 13:29:29 -0300

    chore: Initial linters config

---
+++ eslint.config.js
@@ -0,0 +1,99 @@
+import eslintPluginTypescript from '@typescript-eslint/eslint-plugin';
+import tsEslintParser from '@typescript-eslint/parser';
+import eslintPluginImport from 'eslint-plugin-import';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+
+export default [
+  {
+    ignores: ['config/*'],
+    languageOptions: {
+      parser: tsEslintParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+
+    plugins: {
+      '@typescript-eslint': eslintPluginTypescript,
+      prettier: eslintPluginPrettier,
+      import: eslintPluginImport,
+    },
+
+    rules: {
+      'sort-imports': [
+        'error',
+        { ignoreCase: true, ignoreDeclarationSort: true },
+      ],
+      'import/order': [
+        'error',
+        {
+          groups: [
+            ['external', 'builtin'],
+            'internal',
+            ['sibling', 'parent'],
+            'index',
+          ],
+          pathGroups: [
+            {
+              pattern: '@modules/**',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '@config/**',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '@shared/**',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '@errors/**',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '@utils/**',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '@types/**',
+              group: 'internal',
+              position: 'after',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+      'prettier/prettier': 'error',
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          ts: 'never',
+        },
+      ],
+      'import/prefer-default-export': 'off',
+      'import/no-unresolved': 'error',
+      'no-shadow': 'off',
+      camelcase: 'off',
+    },
+
+    settings: {
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+        },
+      },
+    },
+  },
+];
