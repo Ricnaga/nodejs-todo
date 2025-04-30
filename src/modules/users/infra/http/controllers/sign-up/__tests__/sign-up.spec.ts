f6ba5ed643e699bdd6b6f38bc7cd96a28714d105
ricnaga <ric.nagato@gmail.com>
2025-04-30 11:02:19 -0300

    test: Improvement test

--- src/modules/users/infra/http/controllers/sign-up/__tests__/sign-up.spec.ts
+++ src/modules/users/infra/http/controllers/sign-up/__tests__/sign-up.spec.ts
@@ -1,15 +1,19 @@
+import { type Response } from 'supertest';
+
 import { signUpRequest } from './sign-up.request';
 
 describe('USERS -> Sign-Up', () => {
-  it('should be able to create a new user', async () => {
-    const response = await signUpRequest();
+  let signUpResponse: Response;
 
-    expect(response.statusCode).toBe(204);
+  beforeAll(async () => {
+    signUpResponse = await signUpRequest();
+  });
+
+  it('should be able to create a new user', async () => {
+    expect(signUpResponse.statusCode).toBe(204);
   });
 
   it('should not be able to create the same user', async () => {
-    await signUpRequest();
-
     const response = await signUpRequest();
 
     expect(response.statusCode).toBe(400);
