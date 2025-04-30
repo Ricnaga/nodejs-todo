75a638a7b893a1c2a5acf4a418f96c4d50058bdb
ricnaga <ric.nagato@gmail.com>
2025-04-30 16:47:36 -0300

    refactor: Update sign up endpoint name

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,23 +1,25 @@
-import validateToken from "@shared/infra/http/middlewares/validate-token.middleware";
-import { Router } from "express";
-import LoginController from "../controllers/login/login.controller";
-import SignUpController from "../controllers/sign-up/sign-up.controller";
-import UpdateController from "../controllers/update/update.controller";
-import MeController from "../controllers/me/me.controller";
+import { Router } from 'express';
+
+import validateToken from '@shared/infra/http/middlewares/validate-token.middleware';
+
+import LoginController from '../controllers/login/login.controller';
+import MeController from '../controllers/me/me.controller';
+import SignUpController from '../controllers/sign-up/sign-up.controller';
+import UpdateController from '../controllers/update/update.controller';
 
 const usersRouter = Router();
 
 const signUpController = new SignUpController();
 
-usersRouter.post("/sign-up", signUpController.create);
+usersRouter.post('/', signUpController.create);
 
 const loginController = new LoginController();
-usersRouter.post("/login", loginController.create);
+usersRouter.post('/login', loginController.create);
 
 const updateController = new UpdateController();
-usersRouter.put("/:id", validateToken, updateController.update);
+usersRouter.put('/:id', validateToken, updateController.update);
 
 const meController = new MeController();
-usersRouter.get("/me", validateToken, meController.list);
+usersRouter.get('/me', validateToken, meController.list);
 
 export default usersRouter;
