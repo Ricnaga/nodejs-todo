ed038e8951b4c0aae999cad756a85b1a7b687850
ricnaga <ric.nagato@gmail.com>
2025-05-05 18:31:37 -0300

    chore: Add test on lintstaged

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.ts': [() => 'pnpm typecheck', 'pnpm lint:fix'],
+  '*.ts': [() => 'pnpm typecheck', 'pnpm lint:fix', 'pnpm test'],
   '*.{js,ts,json,cjs,mjs}': ['prettier --write'],
 };
